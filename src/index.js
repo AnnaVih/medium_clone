c669db243d7b22cd8a2d4078b0e8ed3194839aab
AnnaVihrogonova <anna.vihrogonova@acuris.com>
2019-12-29 13:23:29 +0000

    get current user on init page

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Routes from 'routes'
 import TopBar from 'components/topBar'
 import { CurrentUserProvider } from 'context/currentUser'
+import CurrentUserChecker from 'components/currentUserChecker'
 
 const App = () => {
   return (
     <CurrentUserProvider>
-      <Router>
-        <TopBar/>
-        <Routes />
-      </Router>
+      <CurrentUserChecker>
+        <Router>
+          <TopBar/>
+          <Routes />
+        </Router>
+      </CurrentUserChecker>
     </CurrentUserProvider>
   )
 }
